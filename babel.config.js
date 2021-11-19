2237760c5250962bad2b7e543f7487df0e4ac2c7
Leslie Wong <79917148leslie@gmail.com>
2021-11-19 14:58:53 +0800

    :racehorse: perf: make umd bundle es5-like

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/env",
+      {
+        modules: false,
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          JSBI: "node_modules/jsbi",
+        },
+      },
+    ],
+    "@babel/plugin-proposal-class-properties",
+  ],
+};
